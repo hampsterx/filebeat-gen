aa906981de5b3f67192b7aaa619568b176242698
Tim <tim.vdh@gmail.com>
2016-11-23 08:37:45 +1300

    include stopped and add wait

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ set -e
 service supervisor start &
 
 
-/opt/docker-gen -notify "supervisorctl restart filebeat" -notify-output -watch /etc/docker-gen/filebeat.tmpl /tmp/filebeat.yml
+/opt/docker-gen -include-stopped -wait 1s:2s -notify "supervisorctl restart filebeat" -notify-output -watch /etc/docker-gen/filebeat.tmpl /tmp/filebeat.yml
 
 
